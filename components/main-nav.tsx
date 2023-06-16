b8418736c69d4e9b49320d0183176a0c57d17cb7
collinw7 <dcwill03@gmail.com>
2023-06-16 17:43:03 -0400

    scrolling and navigation

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { ThemeToggle } from "./ui/theme-toggle";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -9,22 +9,29 @@ import {
 } from "./ui/navigation-menu";
 import Link from "next/link";
 
+const isBrowser = () => typeof window !== "undefined";
+
+function scrollTop() {
+  if (!isBrowser()) return;
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
 const MainNav = (): JSX.Element => {
   return (
     <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full bg-background/80 backdrop-blur">
-      <div className="p-4 flex items-center container">
+      <div className="px-12 flex items-center container">
         <div className="flex w-1/2">
-          <h3>CWSE</h3>
+          <h3 className="py-4 cursor-pointer" onClick={scrollTop}>CWSE</h3>
           <NavigationMenu className="ml-10 justify-start space-x-2">
             <NavigationMenuItem className="list-none">
-              <Link href="#about-me" scroll={false} legacyBehavior passHref>
+              <Link href="#about-me" replace legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                   About Me
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem className="list-none">
-              <Link href="" legacyBehavior passHref>
+              <Link href="" replace legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                   Projects
                 </NavigationMenuLink>
