f1aae65544d281ca2ce5224b61abdfb34be7714c
collinw7 <dcwill03@gmail.com>
2023-06-21 00:12:10 -0400

    favicon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning className="!scroll-smooth">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={inter.className}>
         <Provider>
           <GlobalHeader />
